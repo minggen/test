7f3601388f95968a127ce731f794b4f0d993eef4
王王 <919767736@qq.com>
2017-10-15 19:49:53 +0800

    some

--- t.sh
+++ t.sh
@@ -5,6 +5,6 @@ git commit -a -m "Changed some files"
 
 git remote add origin git@github.com:wmgselfblog/test.git
 
-%git push origin master  可以把 master 换成你想要推送的任何分支。 
+%git push -u origin master  可以把 master 换成你想要推送的任何分支。 
 
-git push;
+git push -u origin master;
