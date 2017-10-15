022064ff34bb5d8c697d1d11d107eaa24e0b3030
?????? <919767736@qq.com>
2017-10-15 19:42:09 +0800

    rr

---
+++ t.sh
@@ -0,0 +1,10 @@
+git init
+git add filename
+git commit -m "Adding files"
+git commit -a -m "Changed some files"
+
+git remote add origin git@github.com:wmgselfblog/test.git
+
+%git push origin master  可以把 master 换成你想要推送的任何分支。 
+
+git push;
